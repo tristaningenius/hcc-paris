6f7e9ee97b2b474d72debc41a3eaff3870e68523
Martin <martin.feuillet92@gmail.com>
2023-05-16 05:55:55 +0200

    correcting minor developments bugs

--- src/pages/500.js
+++ src/pages/500.js
@@ -2,10 +2,8 @@ import Link from 'next/link';
 import { Helmet } from 'react-helmet';
 
 import Layout from 'components/Layout';
-import Section from 'components/Section';
+import { Section } from 'components/elements';
 import Container from 'components/Container';
-
-import styles from 'styles/pages/Error.module.scss';
 
 export default function Custom500() {
   return (
@@ -15,10 +13,10 @@ export default function Custom500() {
         <meta name="robots" content="noindex, nofollow" />
       </Helmet>
       <Section>
-        <Container className={styles.center}>
+        <Container>
           <h1>Internal Error</h1>
-          <p className={styles.errorCode}>500</p>
-          <p className={styles.errorMessage}>
+          <p>500</p>
+          <p>
             Uh oh, something went wrong. Please try refreshing the page or clearing site data. If the problem persists,
             reach out to let us know!
           </p>
